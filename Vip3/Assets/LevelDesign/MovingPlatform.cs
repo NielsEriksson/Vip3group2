d0c0cb41d3f3c8a627d3600907f6105303afaccc
NielsEriksson <erikssonniels98@gmail.com>
2024-05-20 11:51:55 +0200

    moving platform

--- Vip3/Assets/LevelDesign/MovingPlatform.cs
+++ Vip3/Assets/LevelDesign/MovingPlatform.cs
@@ -46,7 +46,8 @@ public class MovingPlatform : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
